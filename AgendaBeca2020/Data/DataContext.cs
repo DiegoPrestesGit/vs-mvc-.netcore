bed323b5a3175b0d1e64048ef56487e98bfef00b
Diego Prestes <diegoprestes.git@hotmail.com>
2020-12-14 11:41:41 -0300

    feat: starting implementing database in C# using the entityframework in memory

---
+++ AgendaBeca2020/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using AgendaBeca2020.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaBeca2020.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Contact> 
+    }
+}
